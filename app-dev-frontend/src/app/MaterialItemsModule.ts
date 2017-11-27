b5c5e4c9ce8ca1bdb7ac964548e93626dbb15497
Aldius <aldius@inf.elte.hu>
2017-11-27 10:36:21 +0100

    "mobil menu"

--- app-dev-frontend/src/app/MaterialItemsModule.ts
+++ app-dev-frontend/src/app/MaterialItemsModule.ts
@@ -7,17 +7,17 @@ import {
   MatToolbarModule,
   MatCardModule,
   MatDialogModule,
-  MatMenuModule, MatListModule, MatPaginatorModule, MatTooltipModule
+  MatMenuModule, MatListModule, MatPaginatorModule, MatTooltipModule, MatSidenavModule
 } from '@angular/material';
 import {NgModule} from "@angular/core";
 
 @NgModule({
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule,
     MatTableModule, MatSelectModule, MatCardModule, MatDialogModule, MatMenuModule,
-    MatListModule, MatPaginatorModule, MatTooltipModule],
+    MatListModule, MatPaginatorModule, MatTooltipModule, MatSidenavModule],
   exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule,
     MatTableModule, MatSelectModule, MatCardModule, MatDialogModule, MatMenuModule,
-    MatListModule, MatPaginatorModule, MatTooltipModule],
+    MatListModule, MatPaginatorModule, MatTooltipModule, MatSidenavModule],
 })
 export class MaterialItemsModule {
 }
